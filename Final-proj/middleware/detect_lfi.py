eafd66c04a4060d7294e835cdb20d69eb37b0eb4
aryan-bhandari <140818067+aryan-bhandari@users.noreply.github.com>
2025-05-13 01:45:36 +0530

    Update detect_lfi.py

--- Final-proj/middleware/detect_lfi.py
+++ Final-proj/middleware/detect_lfi.py
@@ -19,7 +19,7 @@ GEO_API = "http://ip-api.com/json/"
 SENDER_EMAIL = os.getenv("SENDER_EMAIL", "allsafeallsafe612@gmail.com")
 EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD", "htsneaayrqwutldg")
 RECEIVER_EMAILS = os.getenv("RECEIVER_EMAILS", "unknownzero51@gmail.com,aryanbhandari2431@gmail.com").split(",")
-DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL", "https://discord.com/api/webhooks/1368123901993025567/kFmLfrxYKqbAVJAbVWywIxjwQ0ThuoADykdSbwXqY7K_Op6gBVEPvLNdoXl0CXfYHLLL")
+DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL", "https://discord.com/api/webhooks/1371581428738953366/D1dZ3MWbVApZaeW3gJvNsH3pH1kO_m7jM1C0ypk6u-Ou8SPUiP8rvVqeESexoesleuHW")
 
 # ----------------- Trusted Proxies -----------------
 TRUSTED_PROXIES = [
