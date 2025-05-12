7fb3154d81977f80881ca62227a71a61cb61ad7d
aryan-bhandari <140818067+aryan-bhandari@users.noreply.github.com>
2025-05-13 01:46:02 +0530

    Update content_detect.py

--- Final-proj/middleware/content_detect.py
+++ Final-proj/middleware/content_detect.py
@@ -14,7 +14,7 @@ GENERAL_LOG = 'logs/general.log'
 SENDER_EMAIL = os.getenv("SENDER_EMAIL", "allsafeallsafe612@gmail.com")
 EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD", "htsneaayrqwutldg")
 RECEIVER_EMAILS = os.getenv("RECEIVER_EMAILS", "unknownzero51@gmail.com,aryanbhandari2431@gmail.com").split(",")
-DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL", "https://discord.com/api/webhooks/1368123901993025567/kFmLfrxYKqbAVJAbVWywIxjwQ0ThuoADykdSbwXqY7K_Op6gBVEPvLNdoXl0CXfYHLLL")
+DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL", "https://discord.com/api/webhooks/1371581428738953366/D1dZ3MWbVApZaeW3gJvNsH3pH1kO_m7jM1C0ypk6u-Ou8SPUiP8rvVqeESexoesleuHW")
 
 # Trusted proxies like Cloudflare
 TRUSTED_PROXIES = [
